fa9a7428a8ffd248f32e738e6d93fc46576b3672
Gugoraaa <gus.gonzalez.ramos@gmail.com>
2025-01-25 01:54:49 -0700

    big color rebranding

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ const tabContents = document.querySelectorAll(".tab-content");
 
 function activateTab(tab, index) {
   tabs.forEach(t => {
-    t.classList.remove("active", "text-[#63A4D3]", "border-b-2", "border-[#2A9D8F]", "text-black", "rounded-lg","font-bold","bg-blue-400");
+    t.classList.remove("active", "text-white", "border-b-2", "border-[#2A9D8F]", "rounded-lg","font-bold","bg-blue-400", "text-black");
   });
   tab.classList.add("active", "text-black", "border-b-2", "border-[#2A9D8F]", "rounded-lg","font-bold","bg-blue-400");
   tabContents.forEach(content => content.classList.add("hidden"));
